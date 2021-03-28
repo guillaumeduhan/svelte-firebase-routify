cf287e4d43409bc9b91681a425fbba3df886a4c4
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2021-03-28 14:15:37 +0100

    Delete config.js

--- src/auth/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCHjoz9ZeE3YlvwMmxMCUyouPnu3_MpCiE",
-  authDomain: "medtech-app-3b36b.firebaseapp.com",
-  databaseURL: "https://medtech-app-3b36b.firebaseio.com",
-  projectId: "medtech-app-3b36b",
-  storageBucket: "medtech-app-3b36b.appspot.com",
-  messagingSenderId: "903512650499",
-  appId: "1:903512650499:web:79db4812c27b86559bef36"
-}
